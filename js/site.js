9b2edfd0b43f74c2c1c99f5314c1c8905a32bac6
Tercan Keskin <tercankeskin@gmail.com>
2021-02-01 19:31:23 +0300

    First commit

---
+++ js/site.js
@@ -0,0 +1,13 @@
+/* --------------------------------------------------
+ * Miscellaneous JavaScript Definitions
+ * Author: Tercan Keskin
+ * Date: 2021-02-01
+ * Copyright (c) 2021
+ * Dual licensed under the MIT and GPL licenses.
+-------------------------------------------------- */
+
+
+jQuery(document).ready(function($) {
+
+
+});
